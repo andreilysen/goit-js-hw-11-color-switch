e7e188b55d793ee82d2a319620b69248d6019a5a
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-05-23 15:06:49 +0300

    правка кода

--- js/index.js
+++ js/index.js
@@ -13,23 +13,30 @@ const body = document.querySelector("body");
 const startBtn = document.querySelector('[data-action="start"]');
 const stopBtn = document.querySelector('[data-action="stop"]');
 let intervalId = null;
+console.log(intervalId);
 
 const bodyColor = (arr, random) => {
   const index = random(0, arr.length - 1);
-  const color = arr[index];
   body.style.backgroundColor = arr[index];
 };
 
 const changeBodyColor = () => {
+  if (intervalId !== null) {
+    return;
+  }
   intervalId = setInterval(() => {
     bodyColor(colors, randomIntegerFromInterval);
   }, 1000);
   startBtn.disabled = true;
+  console.log(intervalId);
 };
 
 const stopChange = () => {
   clearInterval(intervalId);
   startBtn.disabled = false;
+  intervalId = null;
+
+  console.log(intervalId);
 };
 
 startBtn.addEventListener("click", changeBodyColor);
